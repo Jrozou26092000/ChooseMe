be1913cca31802563933d7e8588e5c69cd583c69
Juan Correa <jumcorrealo@unal.edu.co>
2021-04-06 18:47:29 -0500

    structure
    structure packages

---
+++ Proyect/chooseme/src/main/java/com/chooseme/proyect/ChoosemeApplication.java
@@ -0,0 +1,13 @@
+package com.chooseme.proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoosemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoosemeApplication.class, args);
+	}
+
+}
