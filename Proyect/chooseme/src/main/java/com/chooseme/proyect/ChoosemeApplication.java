b222b76552559029ffa7c0cb081168416ddca265
Juan <jrozou@unal.edu.co>
2021-04-11 12:13:19 -0500

    Error CORS spring boot security.

--- Proyect/chooseme/src/main/java/com/chooseme/proyect/ChoosemeApplication.java
+++ Proyect/chooseme/src/main/java/com/chooseme/proyect/ChoosemeApplication.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.chooseme.proyect.entities.Users;
 import com.chooseme.proyect.repository.UsersRepository;
@@ -22,5 +26,15 @@ public class ChoosemeApplication {
 		
 		
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+            }
+        };
+    }
 
 }
